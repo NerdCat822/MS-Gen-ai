ad6df15de4ebe89946391443ec07c00687229550
Nerdcat <gusrbaos33@gmail.com>
2024-04-14 07:20:20 +0900

    5

--- backend.py
+++ backend.py
@@ -36,7 +36,7 @@ def post_summarize(input_text: InputText):
 @app.post("/check_list")
 def post_check_list(input_text: InputText):
     check_list = fewshot_checklist(input_text.text)
-    return {"check-list": check_list}
+    return {"checkList": check_list}
 
 @app.post("/bad2good")
 def post_bad2good(input_text: InputText):
@@ -46,7 +46,7 @@ def post_bad2good(input_text: InputText):
 @app.post("/why_bad_request")
 def post_bad_request(input_text: InputText):
     bad_request = RAG_bad_request()
-    return {"bad-request": bad_request}
+    return {"badRequest": bad_request}
 
 @app.post("/valid_flag")
 def post_valid_flag(input_text: InputText):
